6639f99bbdce83a77a8e33c16f2289053118bf7e
Tyler Mok <tymok06@gmail.com>
2023-10-13 09:40:17 -0500

    classmates app commit 1

---
+++ classmatesApp/Classmates.swift
@@ -0,0 +1,26 @@
+//
+//  Classmates.swift
+//  classmatesApp
+//
+//  Created by TYLER MOK on 10/13/23.
+//
+
+import Foundation
+
+public class Classmates {
+    
+    var nickName: String = ""
+    var name: String = ""
+    var cash: Double = 0.0
+    
+    
+    init(nickName: String, name: String, cash: Double) {
+        self.nickName = nickName
+        self.name = name
+        self.cash = cash
+    }
+    
+    
+    
+    
+}
--- classmatesApp/ViewController.swift
+++ classmatesApp/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var classmates : [Classmates] = []
+        
+        var classmate1 = Classmates.init(nickName: "Musty Mason", name: "Mason", cash: 3.44)
+        classmates.append(classmate1)
+        var classmate2 = Classmates.init(nickName: "Narco Noel", name: "Noel", cash: 123.17)
+        classmates.append(classmate2)
+        var classmate3 = Classmates.init(nickName: "O-Line Oliver", name: "Oliver", cash: 23.72)
+        classmates.append(classmate3)
+        
+        print(classmates)
     }
-
-
+    
+    
+    
+    @IBAction func nextButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func sortButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func quizButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "" {
+            
+            
+            let nvc = segue.destination as! ViewController2
+        }
+     //   else if segue.destination as! ViewController2
+            }
 }
-
